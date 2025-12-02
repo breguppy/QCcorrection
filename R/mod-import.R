390926df6d134c3a26c70880e4aedeb84912d0dc
Guppy <bguppy@healthcare.uiowa.edu>
2025-12-02 13:37:17 -0600

    duplicate metabolite columns detected
    * printed in basic info summary
    * in final pdf report

--- R/mod-import.R
+++ R/mod-import.R
@@ -163,7 +163,7 @@ mod_import_server <- function(id) {
     output$basic_info <- renderUI({
       cd <- cleaned_r()
       req(cd)
-      ui_basic_info(cd$df, cd$replacement_counts, cd$non_numeric_cols)
+      ui_basic_info(cd$df, cd$replacement_counts, cd$non_numeric_cols, cd$duplicate_mets)
     })
     
     filtered_r <- reactive({
